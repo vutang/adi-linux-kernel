969d440242297dd378a983315727d202f76df5dc
Vu Tang <tangvubk@gmail.com>
2024-02-02 16:53:49 +0700

    Print to debug clk rate (more1)

--- drivers/iio/jesd204/axi_jesd204_tx.c
+++ drivers/iio/jesd204/axi_jesd204_tx.c
@@ -627,7 +627,7 @@ static int axi_jesd204_tx_jesd204_link_pre_setup(struct jesd204_dev *jdev,
 	}
 
 	rate = clk_round_rate(jesd->lane_clk, lane_rate);
-	dev_dbg(dev, "%s: Link%u round lane rate %lu returned %ld\n",
+	dev_info(dev, "%s: Link%u round lane rate %lu returned %ld\n",
 		__func__, lnk->link_id, lane_rate, rate);
 
 	if (rate != (long)lane_rate) {
@@ -640,12 +640,12 @@ static int axi_jesd204_tx_jesd204_link_pre_setup(struct jesd204_dev *jdev,
 		parent = clk_get_parent(jesd->lane_clk);
 		rate = clk_get_rate(parent);
 
-		dev_dbg(dev, "%s: Link%u lane parent rate %ld link_rate %ld\n",
+		dev_info(dev, "%s: Link%u lane parent rate %ld link_rate %ld\n",
 			__func__, lnk->link_id, rate, link_rate);
 
 		if (rate != (long)link_rate) {
 			rate = clk_round_rate(parent, link_rate);
-			dev_dbg(dev, "%s: Link%u round lane parent rate %ld\n",
+			dev_info(dev, "%s: Link%u round lane parent rate %ld\n",
 				__func__, lnk->link_id, rate);
 
 			if (rate == (long)link_rate) {
