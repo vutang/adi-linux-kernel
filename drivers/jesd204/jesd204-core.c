d84c7f1b4f52d030635dbdedf004460363fae7f6
Vu Tang <tangvubk@gmail.com>
2024-02-02 22:33:01 +0700

    printk to fix lane rate

--- drivers/jesd204/jesd204-core.c
+++ drivers/jesd204/jesd204-core.c
@@ -193,7 +193,7 @@ int jesd204_link_get_rate(struct jesd204_link *lnk, u64 *lane_rate_hz)
 
 	*lane_rate_hz = rate;
 
-	printk("%s: lane_rate_hz %d\n", __func__, rate);
+	printk("%s: lane_rate_hz %ld\n", __func__, rate);
 
 	return 0;
 }
@@ -251,7 +251,7 @@ int jesd204_link_get_device_clock(struct jesd204_link *lnk,
 
 	*device_clock = lane_rate_hz;
 
-	printk("%s: device_clock %d\n", __func__, lane_rate_hz);
+	printk("%s: device_clock %ld\n", __func__, lane_rate_hz);
 
 	return ret;
 }
